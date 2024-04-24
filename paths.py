648a114fce1fb3d1247d11ddb48209055cf9622f
@20224780 <j.j.malinowski@student.tue.nl>
2024-04-24 21:54:19 +0200

    paths.py created for managing paths between different people. requirements.txt added to keep track of libraries we use (remember to update as you go!)

---
+++ paths.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def get_path_to_data(user: str) -> str:
+    """
+    Returns the path to data of a given person
+    :param user: name of the person working with the data
+    :return:
+    """
+    if user == 'Kuba':
+        return "D:/DC2_data"
+        # TODO everyone should add their path to data :)
+
+
+PAS_path = get_path_to_data('Kuba') + '/PAS_T&Cdashboard_to Q3 23-24.xlsx'
+PAS_csv = pd.read_csv(r"D:/DC2_data/PAS_T&Cdashboard_to Q3 23-24.xlsx")
